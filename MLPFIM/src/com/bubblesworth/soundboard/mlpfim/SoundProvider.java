27e51b3299556f175c7d63fd2a728230cf0cbe85
Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
2011-08-24 03:58:01 +1000

    Use XML to create IDs rather than asset-scans
    This allows the IDs for specific sounds to remain stable as new audio
    is added and old audio removed.
    
    It has the added benefit of (on my HTC Dream, anyway) appearing to
    start a bit faster.
    
    However, the query list isn't sorted anymore, so the sound list is
    pretty much in random at the momemnt.

--- MLPFIM/src/com/bubblesworth/soundboard/mlpfim/SoundProvider.java
+++ MLPFIM/src/com/bubblesworth/soundboard/mlpfim/SoundProvider.java
@@ -5,6 +5,10 @@ package com.bubblesworth.soundboard.mlpfim;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
 
 import org.xmlpull.v1.XmlPullParserException;
 
@@ -33,17 +37,16 @@ public class SoundProvider extends ContentProvider implements SoundColumns {
 
 	private static final String AUTHORITY = "com.bubblesworth.soundboard.mlpfim.soundprovider";
 
-	private static final String SOUNDDIR = "pony sounds v4";
-
 	private class SoundInfo {
 		public int id;
-		public String category;
 		public String track;
 		public String description;
 		public int iconResource;
 	};
 
-	private SoundInfo sounds[];
+	private boolean loaded; // Was any data loaded?
+	private Map<Integer, SoundInfo> sounds = null; // null if we need to try and
+													// load data
 
 	private static final int TRACKS = 1;
 	private static final int TRACKS_ID = 2;
@@ -110,104 +113,159 @@ public class SoundProvider extends ContentProvider implements SoundColumns {
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	// This is what Android allows in filenames that're referenced in R.drawable
-	// Note that '.' is also not allowed, as Android's only allowing that for
-	// the extension, it's stripped for things going into R.drawable
-	private String idifyName( String name ) {
-		return name.toLowerCase().replaceAll("[^a-z0-9_]", "");
-	}
 
 	/* (non-Javadoc)
 	 * @see android.content.ContentProvider#onCreate()
 	 */
 	@Override
 	public boolean onCreate() {
-		try {
-			AssetManager assets = getContext().getAssets();
-			Resources resources = getContext().getResources();
-			int soundCount = 0;
-			for (String category: assets.list(SOUNDDIR)) {
-				soundCount+= assets.list(SOUNDDIR + "/" + category).length;
-			}
-			sounds = new SoundInfo[soundCount];
-			soundCount = 0;
-			for (String category: assets.list(SOUNDDIR)) {
-				String categoryId = idifyName(category);
-				int descriptionXmlResource = resources.getIdentifier( "xml/" + categoryId, null, "com.bubblesworth.soundboard.mlpfim" );
-				XmlResourceParser xrp = null;
-				String categoryName = category;
-				try{
-					xrp = resources.getXml( descriptionXmlResource );
-					int eventType = xrp.getEventType();
-					// Skip anything before the first tag
-					while (eventType != XmlResourceParser.START_TAG) {
-						eventType = xrp.next();
-					}
-					// Expect that we just hit a category tag
-					xrp.require(XmlResourceParser.START_TAG, null, "category");
-					// Check the category id
-					String tagId = xrp.getAttributeValue(null, "id"); 
-					if ( !tagId.equals( categoryId ) ) {
-						throw new XmlPullParserException( "Got id " + tagId + " but expected id " + categoryId + " (" + xrp.getPositionDescription() + ")", xrp, null);
-					}
-					// Grab the description element
-					categoryName = xrp.getAttributeValue(null, "description");
-				} catch (NotFoundException e) {
-					Log.e(TAG, "Failed to find category description resource", e);
-				} catch (XmlPullParserException e) {
-					Log.e(TAG, "Failed to parse category description", e);
-					xrp.close();
-					xrp = null;
-				}
-				int iconResource = resources.getIdentifier( "drawable/cat_" + categoryId, null, "com.bubblesworth.soundboard.mlpfim" );
-				for (String track: assets.list(SOUNDDIR + "/" + category) ) {
-					sounds[soundCount] = new SoundInfo();
-					sounds[soundCount].id = soundCount;
-					sounds[soundCount].category = category;
-					sounds[soundCount].track = track;
-					sounds[soundCount].iconResource = iconResource;
+		loaded = false;
+		return true;
+	}
 
-					int dotPos = track.lastIndexOf(".");
-					String description = track.substring(0, dotPos);
-					String trackId = idifyName(description);
-					if (xrp != null) {
-						try {
-							xrp.nextTag();
-							xrp.require(XmlResourceParser.START_TAG, null, "description" );
-							String tagId = xrp.getAttributeValue(null, "id"); 
-							if ( !tagId.equals( trackId ) ) {
-								throw new XmlPullParserException( "Got id " + tagId + " but expected id " + trackId + " (" + xrp.getPositionDescription() + ")", xrp, null);
-							}
-							description = xrp.nextText();
-							xrp.require(XmlResourceParser.END_TAG, null, "description");
-						} catch (XmlPullParserException e) {
-							Log.e(TAG, "Failed to parse sound description for trackId " + trackId, e);
-							xrp.close();
-							xrp = null;
-						}
-					}
-					if (iconResource == 0)
-						description = categoryName + " - " + description;
-					sounds[soundCount].description = description;
-					soundCount++;
-				}
-				if (xrp != null) {
-					try {
-						xrp.nextTag();
-						xrp.require(XmlResourceParser.END_TAG, null, "category");
-					} catch (XmlPullParserException e) {
-						Log.e(TAG, "Didn't finish the category file", e);
-					}
-					xrp.close();
-					xrp = null;
-				}
-			}
-		} catch (IOException e) {
+	private boolean loadData() {
+		if (sounds != null)
+			return loaded;
+
+		sounds = new HashMap<Integer, SoundInfo>();
+		loaded = loadSounds(R.xml.sounds);
+		return loaded;
+	}
+
+	private boolean loadSounds(int soundsResource) {
+		Resources resources = getContext().getResources();
+
+		// Load our sounds.xml definition file.
+		XmlResourceParser soundParser = null;
+		try {
+			soundParser = resources.getXml(soundsResource);
+		} catch (NotFoundException e) {
 			Log.e(TAG, "onCreate", e);
-			sounds = new SoundInfo[0];
+			return loaded;
 		}
-		return false;
+
+		try {
+			int eventType = soundParser.getEventType();
+			// Skip anything before the first tag
+			while (eventType != XmlResourceParser.START_TAG) {
+				eventType = soundParser.next();
+			}
+			// Expect that we just hit a "sounds" tag
+			soundParser.require(XmlResourceParser.START_TAG, null, "sounds");
+			String baseDir = soundParser.getAttributeValue(null, "src");
+			eventType = soundParser.next();
+			while (eventType != XmlResourceParser.END_TAG) {
+				soundParser.require(XmlResourceParser.START_TAG, null,
+						"category");
+				String categoryId = soundParser.getAttributeValue(null, "id");
+				int categoryValue = soundParser.getAttributeIntValue(null,
+						"value", -1);
+				assert categoryValue >= 0;
+				soundParser.nextTag();
+
+				try {
+					loadCategory(baseDir, categoryId, categoryValue);
+				} catch (Exception e) {
+					// Log.e called later with this new exception doesn't
+					// output the stacktrace from the chained exception.
+					// So spam the logs a little...
+					Log.e(TAG, "Error in loadCategory for " + categoryId, e);
+					throw new XmlPullParserException(
+							"Error in loadCategory for " + categoryId + " ( "
+									+ categoryValue + " ): ("
+									+ soundParser.getPositionDescription()
+									+ ")", soundParser, e);
+				}
+				soundParser
+						.require(XmlResourceParser.END_TAG, null, "category");
+				eventType = soundParser.next();
+			}
+			soundParser.require(XmlResourceParser.END_TAG, null, "sounds");
+			eventType = soundParser.next();
+		} catch (XmlPullParserException e) {
+			Log.e(TAG, "Failed to parse sounds description " + soundsResource,
+					e);
+			loaded = false;
+			return loaded;
+		} catch (IOException e) {
+			Log.e(TAG, "Failed to parse sounds description " + soundsResource,
+					e);
+			loaded = false;
+			return loaded;
+		} finally {
+			soundParser.close();
+			soundParser = null;
+		}
+		loaded = true;
+		return loaded;
+	}
+
+	// Bubble any exceptions to our caller...
+	private void loadCategory(String baseDir, String categoryId,
+			int categoryValue) throws Exception {
+		AssetManager assets = getContext().getAssets();
+		Resources resources = getContext().getResources();
+
+		// Read the category XML file and ensure the sounds are present, then
+		// add a SoundInfo object to sounds
+		XmlResourceParser catParser = null;
+		int catIconResource = resources.getIdentifier("drawable/cat_"
+				+ categoryId, null, "com.bubblesworth.soundboard.mlpfim");
+		int catXmlResource = resources.getIdentifier("xml/" + categoryId, null,
+				"com.bubblesworth.soundboard.mlpfim");
+		catParser = resources.getXml(catXmlResource);
+		// Cleanup block for catParser
+		try {
+			int eventType = catParser.getEventType();
+			while (eventType != XmlResourceParser.START_TAG) {
+				eventType = catParser.next();
+			}
+			catParser.require(XmlResourceParser.START_TAG, null, "category");
+			String tagId = catParser.getAttributeValue(null, "id");
+			if (!tagId.equals(categoryId)) {
+				throw new XmlPullParserException("Got id " + tagId
+						+ " but expected id " + categoryId + " ("
+						+ catParser.getPositionDescription() + ")", catParser,
+						null);
+			}
+			String catDir = catParser.getAttributeValue(null, "src");
+			HashSet<String> soundFiles = new HashSet<String>(
+					Arrays.asList(assets.list(baseDir + "/" + catDir)));
+			eventType = catParser.next();
+			while (eventType != XmlResourceParser.END_TAG) {
+				catParser.require(XmlResourceParser.START_TAG, null, "sound");
+				String soundFile = catParser.getAttributeValue(null, "src");
+				int soundValue = catParser.getAttributeIntValue(null, "value",
+						-1);
+				assert soundValue >= 0;
+				assert soundValue < 1000;
+				String soundDesc = catParser.nextText();
+
+				if (!soundFiles.contains(soundFile + ".mp3")) {
+					throw new FileNotFoundException(baseDir + "/" + catDir + "/" + soundFile + ".mp3");
+				}
+
+				SoundInfo info = new SoundInfo();
+				info.id = categoryValue * 1000 + soundValue;
+				info.track = baseDir + "/" + catDir + "/" + soundFile + ".mp3";
+				info.iconResource = catIconResource;
+				if (catIconResource == 0)
+					info.description = catDir + " - " + soundDesc;
+				else
+					info.description = soundDesc;
+				Integer key = new Integer(info.id);
+				assert !sounds.containsKey(key);
+				sounds.put(key, info);
+
+				catParser.require(XmlResourceParser.END_TAG, null, "sound");
+				eventType = catParser.next();
+			}
+			catParser.require(XmlResourceParser.END_TAG, null, "category");
+			catParser.next();
+		} finally {
+			catParser.close();
+			catParser = null;
+		}
 	}
 
 	/* (non-Javadoc)
@@ -217,6 +275,9 @@ public class SoundProvider extends ContentProvider implements SoundColumns {
 	public Cursor query(Uri uri, String[] projection, String selection, String[] selectionArgs,
 			String sortOrder) {
 		//Log.d(TAG, "query(" + uri.toString() + ", " + projection.toString());
+		if (!loadData())
+			return null;
+
 		int match = URI_MATCHER.match(uri);
 		switch (match) {
 		case TRACKS:
@@ -240,8 +301,8 @@ public class SoundProvider extends ContentProvider implements SoundColumns {
 			projection = new String[] { _ID, _COUNT, DESCRIPTION, ACTION, ASSET, ICON };
 		}
 		// TODO: Selection and sorting
-		MatrixCursor result = new MatrixCursor(projection, sounds.length);
-		for (SoundInfo sound:sounds) {
+		MatrixCursor result = new MatrixCursor(projection, sounds.size());
+		for (SoundInfo sound : sounds.values()) {
 			MatrixCursor.RowBuilder row = result.newRow();
 			populateRow(row, projection, sound);
 		}
@@ -251,9 +312,16 @@ public class SoundProvider extends ContentProvider implements SoundColumns {
 	}
 
 	private Cursor queryTrack(long id, String[] projection) {
-		if (id >= sounds.length || id < 0)
+		assert id <= Integer.MAX_VALUE && id >= Integer.MIN_VALUE : id;
+		Integer key = new Integer((int) id);
+		if (!sounds.containsKey(key))
 			return null;
-		SoundInfo sound = sounds[(int)id];
+
+		if (projection == null) {
+			projection = new String[] { _ID, _COUNT, DESCRIPTION, ACTION,
+					ASSET, ICON };
+		}
+		SoundInfo sound = sounds.get(key);
 		MatrixCursor result = new MatrixCursor(projection, 1);
 		MatrixCursor.RowBuilder row = result.newRow();
 		populateRow(row, projection, sound);
@@ -294,17 +362,22 @@ public class SoundProvider extends ContentProvider implements SoundColumns {
 			throws FileNotFoundException {
 		if (!mode.equals("r"))
 			throw new FileNotFoundException();
-		long id = ContentUris.parseId(uri);
-		if (id >= sounds.length || id < 0)
+		if (!loadData())
 			throw new FileNotFoundException();
-		SoundInfo sound = sounds[(int)id];
+
+		long id = ContentUris.parseId(uri);
+		assert id <= Integer.MAX_VALUE && id >= Integer.MIN_VALUE : id;
+		Integer key = new Integer((int) id);
+		if (!sounds.containsKey(key))
+			throw new FileNotFoundException();
+
+		SoundInfo sound = sounds.get(key);
 
 		int match = URI_MATCHER.match(uri);
 		switch (match) {
 			case ASSETS_ID:
-				String assetPath = SOUNDDIR + "/" + sound.category + "/" + sound.track;
 				try {
-					return getContext().getAssets().openFd(assetPath);
+					return getContext().getAssets().openFd(sound.track);
 				} catch (IOException e) {
 					Log.e(TAG, "openAssetFileDescriptor", e);
 					throw new FileNotFoundException(e.getLocalizedMessage());
