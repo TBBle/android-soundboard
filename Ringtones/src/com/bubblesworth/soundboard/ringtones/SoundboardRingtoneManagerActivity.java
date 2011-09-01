4738dcffadacf80a801d8d73013aa8e19e81f82b
Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
2011-09-01 19:21:09 +1000

    Make the Ringtone Manager use a context menu
    Deletion is done with a long-press, a tap simply plays the
    sounds.
    
    The Ringtone Manager is now a "Favourites" storage system. Yay!

--- Ringtones/src/com/bubblesworth/soundboard/ringtones/SoundboardRingtoneManagerActivity.java
+++ Ringtones/src/com/bubblesworth/soundboard/ringtones/SoundboardRingtoneManagerActivity.java
@@ -3,13 +3,22 @@
  */
 package com.bubblesworth.soundboard.ringtones;
 
+import java.io.IOException;
+
 import android.app.ListActivity;
 import android.content.ContentUris;
 import android.database.Cursor;
+import android.media.MediaPlayer;
 import android.net.Uri;
 import android.os.Bundle;
 import android.provider.MediaStore;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.ContextMenu.ContextMenuInfo;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView.AdapterContextMenuInfo;
 import android.widget.ListView;
 import android.widget.SimpleCursorAdapter;
 
@@ -18,7 +27,7 @@ import android.widget.SimpleCursorAdapter;
  * 
  */
 public class SoundboardRingtoneManagerActivity extends ListActivity {
-	// private static final String TAG = "SoundboardRingtoneManagerActivity";
+	private static final String TAG = "SoundboardRingtoneManagerActivity";
 
 	/*
 	 * (non-Javadoc)
@@ -38,6 +47,7 @@ public class SoundboardRingtoneManagerActivity extends ListActivity {
 				new String[] { MediaStore.Audio.Media.TITLE },
 				new int[] { android.R.id.text1 });
 		setListAdapter(adapter);
+		registerForContextMenu(getListView());
 	}
 
 	/*
@@ -50,7 +60,56 @@ public class SoundboardRingtoneManagerActivity extends ListActivity {
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		Uri mediaUri = ContentUris.withAppendedId(
 				SoundboardRingtoneFileManager.CONTENT_URI, id);
-		SoundboardRingtoneFileManager.deleteMediaUri(this, mediaUri);
+		MediaPlayer mp = new MediaPlayer();
+		mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+
+			@Override
+			public void onCompletion(MediaPlayer mp) {
+				mp.release();
+			}
+		});
+		try {
+			mp.setDataSource(this, mediaUri);
+			mp.prepare();
+		} catch (IOException e) {
+			Log.e(TAG, "Failed to play back mediaUri " + mediaUri.toString());
+		}
+		mp.start();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onCreateContextMenu(android.view.ContextMenu,
+	 * android.view.View, android.view.ContextMenu.ContextMenuInfo)
+	 */
+	@Override
+	public void onCreateContextMenu(ContextMenu menu, View v,
+			ContextMenuInfo menuInfo) {
+		// TODO Auto-generated method stub
+		super.onCreateContextMenu(menu, v, menuInfo);
+		MenuInflater inflater = getMenuInflater();
+		inflater.inflate(R.menu.manager_context_menu, menu);
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onContextItemSelected(android.view.MenuItem)
+	 */
+	@Override
+	public boolean onContextItemSelected(MenuItem item) {
+		AdapterContextMenuInfo info = (AdapterContextMenuInfo) item
+				.getMenuInfo();
+		Uri mediaUri = ContentUris.withAppendedId(
+				SoundboardRingtoneFileManager.CONTENT_URI, info.id);
+		switch (item.getItemId()) {
+		case R.id.menuDelete:
+			SoundboardRingtoneFileManager.deleteMediaUri(this, mediaUri);
+			return true;
+		default:
+			return super.onContextItemSelected(item);
+		}
 	}
 
 }
