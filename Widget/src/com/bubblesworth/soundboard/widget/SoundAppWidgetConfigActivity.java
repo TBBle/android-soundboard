6d455afb70b13f36e5a04134457727d6a1bc3281
Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
2011-09-02 00:41:35 +1000

    Format SoundAppWidgetConfigActivity using Eclipse

--- Widget/src/com/bubblesworth/soundboard/widget/SoundAppWidgetConfigActivity.java
+++ Widget/src/com/bubblesworth/soundboard/widget/SoundAppWidgetConfigActivity.java
@@ -15,7 +15,7 @@ import android.util.Log;
 
 /**
  * @author tbble
- *
+ * 
  */
 public class SoundAppWidgetConfigActivity extends Activity {
 	private static final String TAG = "SoundAppWidgetConfigActivity";
@@ -31,7 +31,8 @@ public class SoundAppWidgetConfigActivity extends Activity {
 		widgetId = AppWidgetManager.INVALID_APPWIDGET_ID;
 		Bundle extras = getIntent().getExtras();
 		if (extras != null) {
-			widgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID, AppWidgetManager.INVALID_APPWIDGET_ID);
+			widgetId = extras.getInt(AppWidgetManager.EXTRA_APPWIDGET_ID,
+					AppWidgetManager.INVALID_APPWIDGET_ID);
 		}
 		setResult(RESULT_CANCELED);
 		if (widgetId == AppWidgetManager.INVALID_APPWIDGET_ID) {
@@ -41,15 +42,18 @@ public class SoundAppWidgetConfigActivity extends Activity {
 
 		// If we were killed, we already started this activity, and can just
 		// wait for the result.
-		if (savedInstanceState == null || !savedInstanceState.getBoolean(STATEKEY_WASRUNNING))
-		{
+		if (savedInstanceState == null
+				|| !savedInstanceState.getBoolean(STATEKEY_WASRUNNING)) {
 			// Take advantage of the Android activity selection code,
 			// any compatible soundboard should respond to this action with a
 			// ContentProvider URL.
-			// I wonder if or how I can export this...? I guess you just document
-			// it, since APPWIDGET_CONFIG isn't exported from android.appwidget.action
+			// I wonder if or how I can export this...? I guess you just
+			// document
+			// it, since APPWIDGET_CONFIG isn't exported from
+			// android.appwidget.action
 			// either.
-			Intent intent = new Intent("com.bubblesworth.soundboard.APPWIDGET_CONFIGURE");
+			Intent intent = new Intent(
+					"com.bubblesworth.soundboard.APPWIDGET_CONFIGURE");
 			try {
 				startActivityForResult(intent, 0);
 			} catch (ActivityNotFoundException e) {
@@ -61,7 +65,9 @@ public class SoundAppWidgetConfigActivity extends Activity {
 		}
 	}
 
-	/* (non-Javadoc)
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see android.app.Activity#onSaveInstanceState(android.os.Bundle)
 	 */
 	@Override
@@ -70,8 +76,11 @@ public class SoundAppWidgetConfigActivity extends Activity {
 		outState.putBoolean(STATEKEY_WASRUNNING, true);
 	}
 
-	/* (non-Javadoc)
-	 * @see android.app.Activity#onActivityResult(int, int, android.content.Intent)
+	/*
+	 * (non-Javadoc)
+	 * 
+	 * @see android.app.Activity#onActivityResult(int, int,
+	 * android.content.Intent)
 	 */
 	@Override
 	protected void onActivityResult(int requestCode, int resultCode, Intent data) {
@@ -82,7 +91,8 @@ public class SoundAppWidgetConfigActivity extends Activity {
 		Uri asset = data.getData();
 		storeContentURIForWidget(this, widgetId, asset);
 		AppWidgetManager appWidgetManager = AppWidgetManager.getInstance(this);
-		SoundAppWidgetProvider.updateAppWidget(this, appWidgetManager, widgetId, asset);
+		SoundAppWidgetProvider.updateAppWidget(this, appWidgetManager,
+				widgetId, asset);
 
 		// Notify the AppWidgetManager that we finished configuring the widget
 		Intent resultValue = new Intent();
@@ -94,19 +104,24 @@ public class SoundAppWidgetConfigActivity extends Activity {
 	private static final String PREFS_NAME = "com.bubblesworth.soundboard.widget";
 	private static final String PREF_WIDGET_ASSET_KEY = "widget_asset_";
 
-	// This is actually called from SoundAppWidgetProvider, but it's here because
+	// This is actually called from SoundAppWidgetProvider, but it's here
+	// because
 	// it relies on the private data above.
 	// Hopefully this doesn't fail due to Context mismatch?
 	static Uri retreiveContentURIForWidget(Context context, int appWidgetId) {
-		SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
-		String asset = prefs.getString(PREF_WIDGET_ASSET_KEY + appWidgetId, null);
+		SharedPreferences prefs = context.getSharedPreferences(PREFS_NAME,
+				MODE_PRIVATE);
+		String asset = prefs.getString(PREF_WIDGET_ASSET_KEY + appWidgetId,
+				null);
 		if (asset == null)
 			return null;
 		return Uri.parse(asset);
 	}
 
-	static void storeContentURIForWidget(Context context, int appWidgetId, Uri asset) {
-		SharedPreferences.Editor prefs = context.getSharedPreferences(PREFS_NAME, 0).edit();
+	static void storeContentURIForWidget(Context context, int appWidgetId,
+			Uri asset) {
+		SharedPreferences.Editor prefs = context.getSharedPreferences(
+				PREFS_NAME, 0).edit();
 		prefs.putString(PREF_WIDGET_ASSET_KEY + appWidgetId, asset.toString());
 		prefs.commit();
 	}
