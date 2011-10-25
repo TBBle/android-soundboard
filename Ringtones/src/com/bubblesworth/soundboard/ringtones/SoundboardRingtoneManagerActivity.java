c9c8e8014816d38aeb87a55fab7900c8bf9d2df2
Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
2011-10-25 15:51:18 +1100

    Update sound player in Ringtones
    This carries across the various bug fixes made to the use of
    MediaPlayer in SoundboardService across to SoundboardRingtoneManagerActivity
    preventing sounds cutting off due to driver surprises or garbage collection
    of unreferenced MediaPlayer objects.

--- Ringtones/src/com/bubblesworth/soundboard/ringtones/SoundboardRingtoneManagerActivity.java
+++ Ringtones/src/com/bubblesworth/soundboard/ringtones/SoundboardRingtoneManagerActivity.java
@@ -4,15 +4,22 @@
 package com.bubblesworth.soundboard.ringtones;
 
 import java.io.IOException;
+import java.util.HashSet;
 
 import android.app.ListActivity;
 import android.content.ContentUris;
 import android.content.Intent;
 import android.database.Cursor;
 import android.media.MediaPlayer;
+import android.media.MediaPlayer.OnCompletionListener;
+import android.media.MediaPlayer.OnPreparedListener;
 import android.media.RingtoneManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.HandlerThread;
+import android.os.Looper;
+import android.os.Process;
 import android.provider.MediaStore;
 import android.util.Log;
 import android.view.ContextMenu;
@@ -30,8 +37,65 @@ import android.widget.TextView;
  * @author paulh
  * 
  */
-public class SoundboardRingtoneManagerActivity extends ListActivity {
+public class SoundboardRingtoneManagerActivity extends ListActivity implements
+		OnPreparedListener, OnCompletionListener {
 	private static final String TAG = "SoundboardRingtoneManagerActivity";
+
+	private Looper looper;
+	private Handler handler;
+	private HashSet<MediaPlayer> players;
+
+	private class PlayerStarter implements Runnable {
+		Uri mediaUri;
+
+		PlayerStarter(Uri uri) {
+			mediaUri = uri;
+		}
+
+		@Override
+		public void run() {
+			try {
+				MediaPlayer mp = new MediaPlayer();
+				players.add(mp);
+				mp.setDataSource(SoundboardRingtoneManagerActivity.this,
+						mediaUri);
+				mp.setOnCompletionListener(SoundboardRingtoneManagerActivity.this);
+				mp.setOnPreparedListener(SoundboardRingtoneManagerActivity.this);
+				mp.prepareAsync();
+			} catch (IOException e) {
+				Log.e(TAG, "PlayerStarter.run", e);
+			}
+		}
+	}
+
+	@Override
+	public void onPrepared(MediaPlayer mp) {
+		mp.start();
+	}
+
+	@Override
+	public void onCompletion(MediaPlayer mp) {
+		mp.stop();
+		// Wait 500ms for sounds to clear the hardware
+		// Bluetooth devices have been observed reporting
+		// completion up to 200ms early, and users are
+		// reporting similar symptoms on their devices
+		handler.postDelayed(new PlayerRemover(mp), 500);
+	}
+
+	private class PlayerRemover implements Runnable {
+		MediaPlayer player;
+
+		PlayerRemover(MediaPlayer mp) {
+			player = mp;
+		}
+
+		@Override
+		public void run() {
+			player.release();
+			players.remove(player);
+		}
+	}
 
 	private class RingtoneManagerViewBinder implements
 			SimpleCursorAdapter.ViewBinder {
@@ -93,6 +157,20 @@ public class SoundboardRingtoneManagerActivity extends ListActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		SoundboardRingtoneFileManager.validateMediaStore(this);
 		super.onCreate(savedInstanceState);
+
+		// Start up the thread for media playback. Note that we create a
+		// separate thread because the activity runs in the process's
+		// UI thread, which we don't want to block. We also make it
+		// background priority so CPU-intensive work will not disrupt our UI.
+		players = new HashSet<MediaPlayer>();
+		HandlerThread thread = new HandlerThread(TAG + "HandlerThread",
+				Process.THREAD_PRIORITY_BACKGROUND);
+		thread.start();
+
+		// Get the HandlerThread's Looper and use it for our Handler
+		looper = thread.getLooper();
+		handler = new Handler(looper);
+
 		String[] columns = { MediaStore.Audio.Media._ID,
 				MediaStore.Audio.Media.TITLE };
 		Cursor cur = SoundboardRingtoneFileManager.query(this, columns,
@@ -111,6 +189,17 @@ public class SoundboardRingtoneManagerActivity extends ListActivity {
 	/*
 	 * (non-Javadoc)
 	 * 
+	 * @see android.app.Activity#onDestroy()
+	 */
+	@Override
+	protected void onDestroy() {
+		looper.quit();
+		super.onDestroy();
+	}
+
+	/*
+	 * (non-Javadoc)
+	 * 
 	 * @see android.app.ListActivity#onListItemClick(android.widget.ListView,
 	 * android.view.View, int, long)
 	 */
@@ -118,21 +207,7 @@ public class SoundboardRingtoneManagerActivity extends ListActivity {
 	protected void onListItemClick(ListView l, View v, int position, long id) {
 		Uri mediaUri = ContentUris.withAppendedId(
 				SoundboardRingtoneFileManager.CONTENT_URI, id);
-		MediaPlayer mp = new MediaPlayer();
-		mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
-
-			@Override
-			public void onCompletion(MediaPlayer mp) {
-				mp.release();
-			}
-		});
-		try {
-			mp.setDataSource(this, mediaUri);
-			mp.prepare();
-		} catch (IOException e) {
-			Log.e(TAG, "Failed to play back mediaUri " + mediaUri.toString());
-		}
-		mp.start();
+		handler.post(new PlayerStarter(mediaUri));
 	}
 
 	/*
