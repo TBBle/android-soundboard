b317c41bb20495e233b98f27c0d45efa3b696599
Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
2012-01-02 21:30:26 +0700

    Add missing @Override to SoundPackProvider methods

--- MLPFIMPacks/ManeUnicorns/src/com/bubblesworth/soundboard/mlpfim/packs/maneunicorns/SoundPackProvider.java
+++ MLPFIMPacks/ManeUnicorns/src/com/bubblesworth/soundboard/mlpfim/packs/maneunicorns/SoundPackProvider.java
@@ -11,14 +11,17 @@ public class SoundPackProvider extends
 		com.bubblesworth.soundboard.pack.common.SoundPackProvider {
 	private static final String AUTHORITY = "com.bubblesworth.soundboard.mlpfim.packs.maneunicorns";
 
+	@Override
 	protected String getAuthority() {
 		return AUTHORITY;
 	}
 
+	@Override
 	protected int getSoundsResource() {
 		return R.xml.sounds;
 	}
 
+	@Override
 	protected int getCreditsResource() {
 		return R.xml.credits;
 	}
