c2319df567f7f7f54f592757251227afea289c99
Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
2012-01-02 21:30:26 +0700

    Add missing @Override to SoundPackProvider methods

--- MLPFIMPacks/SpecialGuests/src/com/bubblesworth/soundboard/mlpfim/packs/specialguests/SoundPackProvider.java
+++ MLPFIMPacks/SpecialGuests/src/com/bubblesworth/soundboard/mlpfim/packs/specialguests/SoundPackProvider.java
@@ -11,14 +11,17 @@ public class SoundPackProvider extends
 		com.bubblesworth.soundboard.pack.common.SoundPackProvider {
 	private static final String AUTHORITY = "com.bubblesworth.soundboard.mlpfim.packs.specialguests";
 
+	@Override
 	protected String getAuthority() {
 		return AUTHORITY;
 	}
 
+	@Override
 	protected int getSoundsResource() {
 		return R.xml.sounds;
 	}
 
+	@Override
 	protected int getCreditsResource() {
 		return R.xml.credits;
 	}
