e5dc84b2e7d72b3c99e770d75a550d8b8e8658b3
Paul "TBBle" Hampson <Paul.Hampson@Pobox.com>
2012-01-02 22:05:11 +0700

    A few minor whitespace fixes
    I've moved to MOTODEV Studio and also updated to the latest Android
    SDK, and the XML editor seems to have stopped mangling the whitespace
    in my strings and menu files.
    
    Also includes a single absent newline from CreditsProvider.java

--- MLPFIM/src/com/bubblesworth/soundboard/mlpfim/CreditsProvider.java
+++ MLPFIM/src/com/bubblesworth/soundboard/mlpfim/CreditsProvider.java
@@ -4,6 +4,7 @@ import com.bubblesworth.soundboard.pack.common.SoundPackProvider;
 
 public class CreditsProvider extends SoundPackProvider {
 	private static final String AUTHORITY = "com.bubblesworth.soundboard.mlpfim.creditsprovider";
+
 	@Override
 	protected String getAuthority() {
 		return AUTHORITY;
